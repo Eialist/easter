99a72e0b94e5ccec0b7f4edd39a6062b261e9fdd
Eialist <elsinoree@gmail.com>
2024-02-27 09:58:27 +0100

    fixed some text-alignment for mobile

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -6,7 +6,7 @@ export const Jumbotron = () => {
       <img src={chicken} alt="" width={100} />
       <h2 className="display-5">Easter Vocabulary</h2>
       <hr className="my-3"></hr>
-      <p className="lead" style={{ textAlign: "left" }}>
+      <p className="lead">
         Träna mer på orden i Easter Activity Book. Växla mellan svenska och
         engelska på knappen i hörnet. Behöver du hjälp finns ordlistan i ditt
         häfte.
